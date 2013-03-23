385dfc98a113bac382b6262b5cc6f721bd61d1cf
wilbefast <wilbefast@gmail.com>
2013-03-23 10:23:11 +0100

    TileGrid loads 'tilelayer' type layers from map01.lua

--- src/TileGrid.lua
+++ src/TileGrid.lua
@@ -18,24 +18,57 @@ IMPORTS
 --]]------------------------------------------------------------
 
 local Class = require("hump/class")
+local Tile = require("Tile")
 
 --[[------------------------------------------------------------
 TILEGRID CLASS
 --]]------------------------------------------------------------
 
+--[[------------------------------------------------------------
+Initialisation
+--]]
 
 local TileGrid = Class
 {
   init = function(self, mapfile)
+    -- grab the size of the map
     self.width = mapfile.width
     self.height = mapfile.height
+    -- grab the size of the tiles
+    self.tilewidth = mapfile.tilewidth
+    self.tileheight = mapfile.tileheight
+    -- create each layer
+    self.layers = {}
+    for z, layer in ipairs(mapfile.layers) do
+      if type == "tilelayer" then
+        self.layers[z] = {}
+        local data_i = 1
+        for x = 1, self.width do
+          self.layers[z][x] = {}
+          for y = 1, self.height do
+            self.layers[z][x][y] = Tile(layer.data[data_i])
+            data_i = data_i + 1
+          end
+        end
+      end
+    end
   end
 }
+
+--[[------------------------------------------------------------
+Game loop
+--]]
 
 function TileGrid:draw()
     love.graphics.print("I am a TileGrid", 256, 256)
     love.graphics.print(self.width, 256, 300)
     love.graphics.print(self.height, 300, 300)
+    --TODO use sprite batches
 end
+
+
+--[[------------------------------------------------------------
+EXPORT
+--]]------------------------------------------------------------
 
 return TileGrid
