cc43abb034a12846632153970986c03d33ed4f89
Alexandre Verbrugghe <av.verbrugghe@hotmail.fr>
2013-03-23 11:51:11 +0100

    merge

--- src/game.lua
+++ src/game.lua
@@ -1,6 +1,6 @@
 --[[
 (C) Copyright 2013 
-William Dyce, Maxime Ailloud, Alex Averbrugghe, Julien Deville
+William Dyce, Maxime Ailloud, Alex verbrugghe, Julien Deville
 
 All rights reserved. This program and the accompanying materials
 are made available under the terms of the GNU Lesser General Public License
@@ -33,8 +33,7 @@ function state:init()
   self.level = Level()
   self.camera = Camera(0, 0)
   self.player = Player()
-<<<<<<< HEAD
-=======
+
 
   player = Player()
 
@@ -77,7 +76,7 @@ function state:init()
 	}
 
 
->>>>>>> maj scroll
+
 end
 
 
@@ -85,15 +84,14 @@ function state:enter()
 
   -- reset objects
   self.level:load("../assets/maps/map01")
-<<<<<<< HEAD
+
   --TODO reset player position base on level
   self.player.x, self.player.y = 100, 100
   self.camera:lookAt(self.player.x, self.player.y)
-=======
+
   self.camera:lookAt(128, 128) --FIXME look at player
 
 
->>>>>>> maj scroll
 end
 
 
@@ -187,11 +185,7 @@ end
 
 function state:draw()
   love.graphics.print("Game screen", 32, 32)
-<<<<<<< HEAD
-  
-=======
 
->>>>>>> maj scroll
   self.camera:attach()
   	self.level:draw()
   	self.player:draw()
@@ -200,21 +194,20 @@ function state:draw()
   -- level:draw()
   -- player.draw()
 
-<<<<<<< HEAD
+
 --[[
   level:draw()
   player.draw()
 
   love.graphics.rectangle("line",50,50,600,500)
-=======
->>>>>>> maj scroll
+
   love.graphics.rectangle("fill",c1.x,c1.y,c1.tw,c1.th)
   love.graphics.rectangle("fill",c2.x,c2.y,c2.tw,c2.th)
   love.graphics.rectangle("fill",c3.x,c3.y,c3.tw,c3.th)
   love.graphics.rectangle("fill",player.x,player.y,5,5)
-<<<<<<< HEAD
+
 --]]
-=======
+
   
   -- barre de magie et life :
 	love.graphics.print("life : " ,560,150)
@@ -224,7 +217,6 @@ function state:draw()
 	love.graphics.rectangle("fill",x_b1,y_b1,player.life,20)
 	love.graphics.rectangle("fill",x_b2,y_b2,player.magie,20)
 
->>>>>>> maj scroll
 
 end
 
