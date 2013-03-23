93224fc908157b0fbd86728aa7c9bad38f6e7de7
wilbefast <wilbefast@gmail.com>
2013-03-23 14:40:33 +0100

    collision tests in TileGrid

--- src/TileGrid.lua
+++ src/TileGrid.lua
@@ -34,12 +34,12 @@ local TileGrid = Class
   init = function(self, mapfile)
   
     -- grab the size of the map
-    self.width = mapfile.width
-    self.height = mapfile.height
+    self.w = mapfile.width
+    self.h = mapfile.height
     
     -- grab the size of the tiles
-    self.tilewidth = mapfile.tilewidth
-    self.tileheight = mapfile.tileheight
+    self.tilew = mapfile.tilewidth
+    self.tileh = mapfile.tileheight
     
     -- grab the tileset
     self.tilesets = {}
@@ -58,9 +58,9 @@ local TileGrid = Class
         -- the mapfile stores tiles in [row, col] format
         local temp_layer = {}
         local data_i = 1
-        for row = 1, self.height do
+        for row = 1, self.h do
           temp_layer[row] = {}
-          for col = 1, self.width do
+          for col = 1, self.w do
             temp_layer[row][col] = Tile(layer.data[data_i])
             data_i = data_i + 1
           end
@@ -68,9 +68,9 @@ local TileGrid = Class
           
         -- we want them in [x, y] format, so we transpose
         self.layers[z] = {}
-        for x = 1, self.width do
+        for x = 1, self.w do
           self.layers[z][x] = {}
-          for y = 1, self.height do
+          for y = 1, self.h do
             self.layers[z][x][y] = temp_layer[y][x]
           end
         end
@@ -86,27 +86,98 @@ Game loop
 function TileGrid:draw(view)
   
   local start_x = math.max(1, 
-              math.floor(view.x / self.tilewidth))
-  local end_x = math.min(self.width, 
-              start_x + math.ceil(view.w / self.tilewidth))
+              math.floor(view.x / self.tilew))
+  local end_x = math.min(self.w, 
+              start_x + math.ceil(view.w / self.tilew))
   
   local start_y = math.max(1, 
-              math.floor(view.y / self.tileheight))
-  local end_y = math.min(self.height, 
-              start_y + math.ceil(view.h / self.tileheight))
+              math.floor(view.y / self.tileh))
+  local end_y = math.min(self.h, 
+              start_y + math.ceil(view.h / self.tileh))
     
   for x = start_x, end_x do
     for y = start_y, end_y do
       local tset_i = self.layers[1][x][y].type
       if tset_i ~= 0 then
         local img = self.tilesets[tset_i].image
-        love.graphics.draw(img, x * self.tilewidth, 
-                                y * self.tileheight)
+        love.graphics.draw(img, x * self.tilew, 
+                                y * self.tileh)
       end
     end
   end
 
     --TODO use sprite batches
+end
+
+--[[----------------------------------------------------------------------------
+Accessors
+--]]--
+
+function TileGrid:gridToTile(self, x, y, z)
+  z = (z or 1)
+  if self:validGridPos(x, y, z) then
+    return self.layers[z][x][y]
+  else
+    return nil --FIXME
+  end
+end
+
+function TileGrid:pixelToTile(self, x, y, z)
+  return self:gridToTile(math.floor(x / self.tilew),
+                         math.floor(y / self.tileh), z)
+end
+
+--[[----------------------------------------------------------------------------
+Avoid array out-of-bounds exceptions
+--]]--
+
+
+function TileGrid:validGridPos(self, x, y)
+  return (x >= 1 
+      and y >= 1
+      and x <= self.size.x 
+      and y <= self.size.y) 
+end
+
+function TileGrid:validPixelPos(self, x, y)
+  return (x >= 0
+      and y >= 0
+      and x <= self.size.x*self.tilew
+      and y <= self.size.y*self.tileh)
+end
+
+
+--[[----------------------------------------------------------------------------
+Basic collision tests
+--]]--
+
+function TileGrid:gridCollision(self, x, y)
+  local type = self:gridToTile(x, y).isWall()
+end
+
+function TileGrid:pixelCollision(self, x, y)
+  local tile = self:pixelToTile(x, y)
+  return ((not tile) or tile.isWall())
+end
+
+--[[----------------------------------------------------------------------------
+GameObject collision tests
+--]]--
+
+function TileGrid:collision(self, go, x, y)
+  -- x & y are optional: leave them out to test the object where it actually is
+  x = x or go.x
+  y = y or go.y
+  
+  -- rectangle collision mask, origin is at the top-left
+  return (self:pixelCollision(x,         y) 
+          or self:pixelCollision(x + go.w,  y) 
+          or self:pixelCollision(x,         y + go.h)
+          or self:pixelCollision(x + go.w,  y + go.h))
+end
+
+function TileGrid:collision_next(self, go, dt)
+  return self:collision(go, go.pos.x + go.inertia.x*dt, go.pos.y + go.inertia.y*dt)
 end
 
 
