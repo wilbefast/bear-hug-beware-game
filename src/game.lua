462a9239743d82501dcb4166c36f52ece6c45528
Alexandre Verbrugghe <av.verbrugghe@hotmail.fr>
2013-03-23 19:15:48 +0100

    son test F

--- src/game.lua
+++ src/game.lua
@@ -107,8 +107,7 @@ function state:keypressed(key, uni)
     self.player:magic_change(-5)
 	-- TEST DE SONS :
   elseif key =="f" then
-    --! FIXME MP3 IS NOT SUPPORTED!
-    --degats_subis:play()
+    degats_subis:play()
   elseif key =="g" then
     cri_mort:play()
   elseif key =="h" then 
